34a4bee3f4711fa486409293514eb57c623a3f0c
Ishant <ishantjoshi112@gmail.com>
2026-09-17 00:01:49 +0545

    final i think

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 from groq import Groq
-import ollama
 import os
 
 load_dotenv()
@@ -18,14 +17,6 @@ def ask_groq(message):
     return response.choices[0].message.content
 
 
-#def ask_ollama(message):
-    response = ollama.chat(
-        model="qwen3:8b",
-        messages=[{"role": "user", "content": message}]
-    )
-    return response["message"]["content"]
-
-
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -35,16 +26,12 @@ def home():
 def chat():
     data = request.json
     message = data.get("message", "")
-    model_choice = data.get("model", "groq")
 
     if not message:
         return jsonify({"error": "No message provided"}), 400
 
     try:
-        if model_choice == "ollama":
-            reply = ask_ollama(message)
-        else:
-            reply = ask_groq(message)
+        reply = ask_groq(message)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
